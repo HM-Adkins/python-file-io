ea02f8f1b434337b81bbdb3a115f3ae57c38130c
Hannah M. Adkins <ham0018@auburn.edu>
2026-04-20 08:11:55 -0500

    Editting and adding functions (write_list, main)

--- find_words.py
+++ find_words.py
@@ -2,56 +2,80 @@
 
 import re
 
+def main():
+    import argparse
 
-def find_words_by_root(input_file, output_file, word_root):
+    # Create a command-line parser object
+    parser = argparse.ArgumentParser()
+
+input_file = 'example_input.txt'
+word_root = 'nit'
+
+def find_words_by_root(input_file, word_root):
     """
     Searches an input file for all occurrrences of words containing the root
-    provided and returns a file with a list of each word found and the line
-    number associated with that word.
+    provided and returns a list of each word found and the line number that
+    the word occurs on.
 
     Parameters
     ----------
     input_file : str
         A text file to be searched.
-    output_file : str
-        A file name to write the list of words found to.
     word_root : str
         A string of the root to be searched for.
 
     Returns
     -------
-    file
-        A file with a list of each word found and the line number associated
+    list
+        A list with each word found and the line number associated
         with that word.
 
-    Examples
+    Example
     --------
-    find_words_by_root(input_file.txt, output_file, 'nit')
-    5 nitrogen
-    7 Nitrogenous
-    13 nitrate 
-    15 nitrite
-    21 Nitrogen
-    30 nitrogen
+    >>> find_words_by_root('example_input.txt', 'nit')
+    '1 nitrogen
+    2 Nitrogenous
+    3 nitrate
+    3 nitrate
+    4 nitrite
+    7 Nitrogen
+    8 nitrogen
+    8 nitrogen'
     """
     root_pattern_str = r'[A-Za-z]*' + word_root + '+' + '[A-Za-z]*'
-    root_pattern = re.compile(root_pattern_str)
+    root_pattern = re.compile(root_pattern_str, re.IGNORECASE)
+    output_list = []
 
     with open(input_file, 'r') as in_stream:
-        with open(output_file, 'w') as out_stream:
-            for line in in_stream:
-                match_object = root_pattern.search(in_stream)
-
-                            if match_object:
-                                return match_object.group()
-                            return ''
-
-                line = line.strip()
-                word_list = line.split()
-                word_list.sort()
-                for word in word_list:
-                    out_stream.write(f'{word}\n')
-
-    
+            for line_num, line in enumerate(in_stream, 1):
+                word_list = root_pattern.findall(line)
+                if word_list:
+                    line_word_list = [f"{line_num} {item}" for item in word_list]
+                    output_list.extend(line_word_list)
+            return output_list
 
 
+def write_list(output_list, output_file):
+    """
+    Returns a file with each string from a list on a new line.
+
+    Parameters
+    ----------
+    output_list : list
+        List of strings to write to a file.
+    output_file : str
+        A file name to write the list to.
+
+    Returns
+    -------
+    file
+        A text file with each string from a list on a new line.
+    """     
+    with open(output_file, 'w') as out_stream:
+        for item in output_list:
+             out_stream.write(f'{item}\n')
+
+
+if __name__ == '__main__':
+    main()
+    output_list = find_words_by_root(input_file, word_root)
